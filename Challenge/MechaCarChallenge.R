6c79b85ab9299af4e2249c43d032d521db7848f1
Bren42 <nytech42@gmail.com>
2022-10-20 10:19:39 -0600

    completed the code for the t.test in general and for each lot, also added images to the repo

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -12,3 +12,21 @@ total_summary <- suspension %>% summarize(PSI) %>% summarize(Mean_PSI = mean(PSI
 
 
 lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI = mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), SD = sd(PSI), .groups = "keep")
+
+
+# T Test analysis
+
+t.test(suspension$PSI, mu = 1500)
+
+# t test of PSI per manufacturing lot
+Lot_1 <- subset(suspension, Manufacturing_Lot == "Lot1")
+
+Lot_2 <- subset(suspension, Manufacturing_Lot == "Lot2")
+
+Lot_3 <- subset(suspension, Manufacturing_Lot == "Lot3")
+
+t.test(Lot_1$PSI, mu = 1500)
+
+t.test(Lot_2$PSI, mu = 1500)
+
+t.test(Lot_3$PSI, mu = 1500)
