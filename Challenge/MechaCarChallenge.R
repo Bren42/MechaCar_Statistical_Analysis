ba1162d227f283ebfcb9f7c7dee0722d43a8e395
Bren42 <nytech42@gmail.com>
2022-10-20 09:32:55 -0600

    added the suspension data as well as the PSI and Lot summaries

--- Challenge/MechaCarChallenge.R
+++ Challenge/MechaCarChallenge.R
@@ -2,3 +2,13 @@ library(dplyr)
 mechacar <- read.csv(file = "MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = mechacar)
 summary(lm(vehicle_length ~ vehicle_weight + spoiler_angle + ground_clearance + AWD + mpg, data = mechacar))
+
+
+# supsension coil analysis code
+
+suspension <- read.csv(file = "Suspension_coil.csv", check.names = F, stringsAsFactors = F)
+
+total_summary <- suspension %>% summarize(PSI) %>% summarize(Mean_PSI = mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), SD = sd(PSI), .groups = "keep")
+
+
+lot_summary <- suspension %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI = mean(PSI), Median_PSI = median(PSI), Variance_PSI = var(PSI), SD = sd(PSI), .groups = "keep")
